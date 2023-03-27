cadc10870db8883f5cc449c9598deb9352516965
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 14:14:43 +0530

    emd.cpp updated
    Add comments to increase understanding 
    
    Improve function and variable naming convention in recent standards
    
    Add error checking: The code assumes that the input tensors are of the correct shape and type. However, it does not check this assumption. Adding error checking will help catch mistakes early.
    
    Use const where appropriate: The input tensors are not modified by the functions. Therefore, they should be declared as const to indicate that they are read-only.
    
    Use references instead of pointers: The input tensors are passed as pointers. However, it would be better to pass them as references to make the code more readable and avoid the need for pointer dereferencing.

--- cuda/emd.cpp
+++ cuda/emd.cpp
@@ -1,29 +1,27 @@
 #include <torch/extension.h>
 #include <vector>
 
-int c_fwd(at::Tensor xyz1, at::Tensor xyz2, at::Tensor dist, at::Tensor assignment, at::Tensor price, 
-	                 at::Tensor assignment_inv, at::Tensor bid, at::Tensor bid_increments, at::Tensor max_increments,
-	                 at::Tensor unass_idx, at::Tensor unass_cnt, at::Tensor unass_cnt_sum, at::Tensor cnt_tmp, at::Tensor max_idx, float eps, int iters);
+// Forward declaration of functions
+int emd_forward(const at::Tensor& xyz1, const at::Tensor& xyz2, at::Tensor& dist, at::Tensor& assignment,
+                at::Tensor& price, at::Tensor& assignment_inv, at::Tensor& bid, at::Tensor& bid_increments,
+                at::Tensor& max_increments, at::Tensor& unass_idx, at::Tensor& unass_cnt, at::Tensor& unass_cnt_sum,
+                at::Tensor& cnt_tmp, at::Tensor& max_idx, const float eps, const int iters);
 
-int c_bck(at::Tensor xyz1, at::Tensor xyz2, at::Tensor gradxyz, at::Tensor graddist, at::Tensor idx);
+int emd_backward(const at::Tensor& xyz1, const at::Tensor& xyz2, at::Tensor& gradxyz, at::Tensor& graddist,
+                 const at::Tensor& idx);
 
-
-
-int fwd(at::Tensor xyz1, at::Tensor xyz2, at::Tensor dist, at::Tensor assignment, at::Tensor price, 
-	                 at::Tensor assignment_inv, at::Tensor bid, at::Tensor bid_increments, at::Tensor max_increments,
-	                 at::Tensor unass_idx, at::Tensor unass_cnt, at::Tensor unass_cnt_sum, at::Tensor cnt_tmp, at::Tensor max_idx, float eps, int iters) {
-	return c_fwd(xyz1, xyz2, dist, assignment, price, assignment_inv, bid, bid_increments, max_increments, unass_idx, unass_cnt, unass_cnt_sum, cnt_tmp, max_idx, eps, iters);
-}
-
-int bck(at::Tensor xyz1, at::Tensor xyz2, at::Tensor gradxyz, at::Tensor graddist, at::Tensor idx) {
-
-    return c_bck(xyz1, xyz2, gradxyz, graddist, idx);
-}
-
-
-
-
+// Define Python functions using the C++ functions
+// Add comments to explain the purpose of the functions and their parameters
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &fwd, "emd forward (CUDA)");
-  m.def("backward", &bck, "emd backward (CUDA)");
+  m.def("forward", &emd_forward, "Compute the Earth Mover's Distance forward pass (CUDA)");
+  m.def("backward", &emd_backward, "Compute the Earth Mover's Distance backward pass (CUDA)");
 }
+
+// Compute the Earth Mover's Distance forward pass
+int emd_forward(const at::Tensor& xyz1, const at::Tensor& xyz2, at::Tensor& dist, at::Tensor& assignment,
+                at::Tensor& price, at::Tensor& assignment_inv, at::Tensor& bid, at::Tensor& bid_increments,
+                at::Tensor& max_increments, at::Tensor& unass_idx, at::Tensor& unass_cnt, at::Tensor& unass_cnt_sum,
+                at::Tensor& cnt_tmp, at::Tensor& max_idx, const float eps, const int iters) {
+  // Add error checking to ensure the input tensors are of the correct shape and type
+  TORCH_CHECK(xyz1.dtype() == torch::kFloat32 && xyz2.dtype() == torch::kFloat32 && dist.dtype() == torch::kFloat32 &&
+              assignment.dtype() == torch::kInt32 && price.dtype() == torch::k
