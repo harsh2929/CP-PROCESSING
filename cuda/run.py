864542ba19531290947a68f65d61bff1692c6f50
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 14:17:56 +0530

    updated rub.py
    Added more context to the setup function.

--- cuda/run.py
+++ cuda/run.py
@@ -3,6 +3,27 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name='emd',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Earth Mover\'s Distance implementation using PyTorch and CUDA',
+    long_description='A PyTorch implementation of the Earth Mover\'s Distance (EMD) algorithm using CUDA for GPU acceleration.',
+    url='https://github.com/your_username/emd',
+    license='MIT',
+    keywords='pytorch cuda emd earth mover\'s distance',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C++',
+        'Programming Language :: CUDA',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
     ext_modules=[
         CUDAExtension('emd', [
             'emd.cpp',
@@ -11,4 +32,41 @@ setup(
     ],
     cmdclass={
         'build_ext': BuildExtension
-    })
+    }
+)
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='emd',
+    version='1.0.0',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Earth Mover\'s Distance implementation using PyTorch and CUDA',
+    long_description='A PyTorch implementation of the Earth Mover\'s Distance (EMD) algorithm using CUDA for GPU acceleration.',
+    url='https://github.com/your_username/emd',
+    license='MIT',
+    keywords='pytorch cuda emd earth mover\'s distance',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: C++',
+        'Programming Language :: CUDA',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+    ext_modules=[
+        CUDAExtension('emd', [
+            'emd.cpp',
+            'emd_cuda.cu',
+        ]),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
+)
