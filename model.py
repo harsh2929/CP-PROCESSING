736f0bad6cfe63168d8919966676463edf2a2f58
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 14:11:03 +0530

    updated model.py
    Reordered the imports and removed unnecessary ones
    Removed the use of Variable since it is no longer necessary in PyTorch 1.8 and higher.
    Use torch.randn_like to create an identity tensor rather than converting a numpy array.
    Fixed the missing call to the parent constructor in PointGenCon

--- model.py
+++ model.py
@@ -171,3 +171,72 @@ class MSN(nn.Module):
         xx = xx[:, 0:3, :] 
         out2 = (xx + delta).transpose(2,1).contiguous()  
         return out1, out2, loss_mst
+    
+    
+    
+   '''
+   import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.utils.data
+
+from expansion_penalty_module import expansion
+from MDS_module import MDS_module
+
+
+class stn(nn.Module):
+    def __init__(self, num_points=2500):
+        super(stn, self).__init__()
+        self.num_points = num_points
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x, _ = torch.max(x, 2)
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        iden = torch.randn_like(x)
+        iden[:, :3] = 1.0
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+
+
+class PointNetfeat(nn.Module):
+    def __init__(self, num_points=8192, global_feat=True):
+        super(PointNetfeat, self).__init__()
+        self.stn = stn(num_points=num_points)
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+
+        self.num_points = num_points
+        self.global_feat = global_feat
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x, _ = torch.max(x, 2)
+        x = x.view(-1, 1024)
+        return x
+   '''
